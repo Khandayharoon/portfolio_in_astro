3939db84781e24c7f067a5cd694b8ff91f3959e1
khandayharoon <khandayharoon@gmail.com>
2025-04-20 13:59:51 +0530

    2004

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+// @ts-ignore
+import cloudflare from "@astrojs/cloudflare"; // ✅ Correct import
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react()],
-  output: "static",
+  output: "static", // or omit this line
+  // adapter: cloudflare(), // comment this out for now
 });
