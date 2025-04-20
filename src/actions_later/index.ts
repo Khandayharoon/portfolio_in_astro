4377d942210aa55e3ffbe08de259836544ea007e
khandayharoon <khandayharoon@gmail.com>
2025-04-20 14:09:29 +0530

    done

--- src/actions_later/index.ts
+++ src/actions_later/index.ts
@@ -1,19 +1,19 @@
-import { sendEmail } from "@/lib/send-mail";
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
+// import { sendEmail } from "@/lib/send-mail";
+// import { defineAction } from "astro:actions";
+// import { z } from "astro:schema";
 
-export const server = {
-  contact: defineAction({
-    accept: "form",
-    input: z.object({
-      name: z.string(),
-      email: z.string().email(),
-      msg: z.string(),
-    }),
-    handler: async (input, context) => {
-      await sendEmail(input);
+// export const server = {
+//   contact: defineAction({
+//     accept: "form",
+//     input: z.object({
+//       name: z.string(),
+//       email: z.string().email(),
+//       msg: z.string(),
+//     }),
+//     handler: async (input, context) => {
+//       await sendEmail(input);
 
-      return `Hello, ${input.name}!`;
-    },
-  }),
-};
+//       return `Hello, ${input.name}!`;
+//     },
+//   }),
+// };
